32c564eccf798ddb2d205380e93f9fd0ff56a834
james-orion <jamesorion322@gmail.com>
2024-10-07 10:45:50 -0400

    started tile class

--- tile.py
+++ tile.py
@@ -1,1 +1,11 @@
+# Class representing a Carcassonne tile
 
+class Tile:
+    # Fields representing the area connected on each side of the tile
+    # From the options of city, road, field
+    # Optionally tile contains monastery or village
+    def __init__(self, top, bottom, left, right):
+        self.top = top
+        self.bottom = bottom
+        self.left = left
+        self.right = right
