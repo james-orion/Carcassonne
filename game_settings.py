198ce1b4738e036000688a47a33102e8fb353b9a
awrigh <abigail.wright@uvm.edu>
2024-10-22 21:17:20 -0400

    edited comments

--- game_settings.py
+++ game_settings.py
@@ -11,28 +11,28 @@ class game_settings:
         self.player_count = 0
 
     def set_current_round(self, round):
-        """ This sets the curent tile placement"""
+        """ This sets the curent round """
         self.current_round= round
 
 
     def add_current_players(self, player):
-        """ This adds"""
+        """ This adds players """
         self.current_players.append(player)
 
     def set_player_count(self, count):
-        """ This sets the moved boolean"""
+        """ This sets player count"""
         self.player_count = count
 
     def get_current_round(self):
-        """"   This returns the current tile placement x"""
+        """"   This returns the current round"""
         return self.current_round
 
     def get_current_players(self):
-        """ This returns the current tile placement y"""
+        """ This returns the current players"""
         return self.current_players
 
     def get_player_count(self):
-        """"   This returns the moved boolean """
+        """"   This returns player count """
         return self.player_count
 
 
